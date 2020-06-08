226648160fb3902d4c12d6dbdaddc943e8fd9a4f
Black-Hole <158blackhole@gmail.com>
2020-06-08 20:35:11 +0800

    refactor(render): perfect react hot reload

--- packages/render-app/config/webpack.config.js
+++ packages/render-app/config/webpack.config.js
@@ -129,6 +129,7 @@ module.exports = function(webpackEnv) {
   };
 
   return {
+    target: 'electron-renderer',
     mode: isEnvProduction ? 'production' : isEnvDevelopment && 'development',
     // Stop compilation early in production
     bail: isEnvProduction,
@@ -289,6 +290,8 @@ module.exports = function(webpackEnv) {
         .map(ext => `.${ext}`)
         .filter(ext => useTypeScript || !ext.includes('ts')),
       alias: {
+        // Support Hot Reload
+        'react-dom': '@hot-loader/react-dom',
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
