da7f8630e409eac85d40c2052e448211902b5849
BlackHole1 <158blackhole@gmail.com>
2020-06-09 09:43:16 +0800

    refactor(renderer): render-app to renderer-app

--- packages/main-app/scripts/copyRenderBuildCode.js
+++ packages/main-app/scripts/copyRenderBuildCode.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-const renderBuildPath = path.join(__dirname, '..', '..', 'render-app', 'build');
+const renderBuildPath = path.join(__dirname, '..', '..', 'renderer-app', 'build');
 const source = path.join(__dirname, '..', 'static', 'render');
 fs.copySync(renderBuildPath, source);
